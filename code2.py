a1db6484111a93074f2f9359b72c59bbef04bc12
kashish <kashish1188@gmail.com>
2021-04-21 17:10:39 +1000

    Python-code-updated

--- code2.py
+++ code2.py
@@ -1,1 +1,4 @@
-sdcscscsdc
+print ("This is test to run")
+
+for i in range (1,10):
+    print (i)
--- proj2code1.py
+++ proj2code1.py
@@ -1,1 +1,13 @@
-print "This is first test fie"
+print ("This is first test fie")
+for i in range (1,5):
+    print (i)
+    print (i+i)
+
+for i in range (1,5):
+    for x in range (6,10):
+        x=x+i
+        print (x)
+    x=6
+    
+
+ 
